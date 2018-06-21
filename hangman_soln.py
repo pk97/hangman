30c9cc2fccb50afb474242aeef1b248225d87f26
Pulkit Kogta <kogtapulkit@gmail.com>
2018-06-21 18:56:37 +0530

    updated for python3

--- hangman_soln.py
+++ hangman_soln.py
@@ -1,4 +1,4 @@
-from string import *
+rom string import *
 from hangman_lib import *
 
 ## Constants
@@ -27,73 +27,73 @@ def print_guessed():
 			list_of_letters.append(letter)
 		else:
 			list_of_letters.append('-')
-	
-	return join(list_of_letters, '')
+	s = ''
+	return s.join(list_of_letters)
 
 
 ##main game code
-print "Welcome To Hangman"
+print ("Welcome To Hangman")
 
-first_time = lower(raw_input("Is this your first time playing Hangman? (y/n)"))
+first_time = input("Is this your first time playing Hangman? (y/n)").lower()
 
 if first_time=="y":
-	print "The objective of Hangman is to guess a secret word letter by letter."
-	print "If you guess a letter in the word, we'll show you that letter."
-	print "But if you guess wrong, we'll draw part of the hangman's body."
-	print "Don't let his whole body get drawn, or else you lose!"
+	print ("The objective of Hangman is to guess a secret word letter by letter.")
+	print ("If you guess a letter in the word, we'll show you that letter.")
+	print ("But if you guess wrong, we'll draw part of the hangman's body.")
+	print ("Don't let his whole body get drawn, or else you lose!")
 
-print "Great, so you're ready to play. Just two things that might help:"
-print "1) The secret word has", len(secret_word), "letters."
-print "2) It takes", MAX_MISTAKES, "wrong guesses to lose."
-print
-print "Good luck!"
-print
-print "[Press enter when ready to play.]"
-raw_input()     # this just waits for them to press enter... they can type
+print ("Great, so you're ready to play. Just two things that might help:")
+print ("1) The secret word has", len(secret_word), "letters.")
+print ("2) It takes", MAX_MISTAKES, "wrong guesses to lose.")
+print ()
+print ("Good luck!")
+print ()
+print ("[Press enter when ready to play.]")
+input()     # this just waits for them to press enter... they can type
                 # other stuff but it doesn't affect anything.
 
 print_hangman_image(0)
 
 while mistakes_made<MAX_MISTAKES:
 
-	print "The word so far: ", print_guessed()
-	print "Letters guessed so far:",
+	print ("The word so far: ", print_guessed())
+	print ("Letters guessed so far:")
 
 	for letter in letters_guessed:
-		print letter,
+		print (letter)
 
-	print "Wrong guesses remaining:", MAX_MISTAKES-mistakes_made
+	print ("Wrong guesses remaining:", MAX_MISTAKES-mistakes_made)
 
-	guess = lower(raw_input("What letter will you guess"))
+	guess = input("What letter will you guess").lower()
 
 	if guess == "":
-		print "You have to guess something"
+		print ("You have to guess something")
 
 	elif len(guess)>1:
-		print "you can guess only one letter at a time."
+		print ("you can guess only one letter at a time.")
 
 	elif guess in letters_guessed:
-		print "you have already guessed this!"
+		print ("you have already guessed this!")
 
 	else:
 		letters_guessed.append(guess)
 
 		if guess in secret_word:
-			print "Good Guess"
+			print ("Good Guess")
 
 			if word_guessed():
-				print "You Done. Good Job"
+				print ("You Done. Good Job")
 				break
 		else:
 			mistakes_made =  mistakes_made+1
-			print "Sorry No luck"
+			print ("Sorry No luck")
 
 			print_hangman_image(mistakes_made)
 
 if mistakes_made>=MAX_MISTAKES:
-	print "On No, You Lose"
-	print "GAME OVER"
+	print ("On No, You Lose")
+	print ("GAME OVER")
 else:
-	print "Congrats"
+	print ("Congrats")
 
-print "The word was: ",secret_word
+print ("The word was: ",secret_word)
